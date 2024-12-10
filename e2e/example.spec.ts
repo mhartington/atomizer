c528213e93c15ad9a70326f0400cda87945b6ca8
Mike Hartington <mikehartington@gmail.com>
2024-12-10 13:59:04 -0500

    stablelish

---
+++ e2e/example.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('Load first page', async ({ page }) => {
+  await page.goto('/todos?q=in-progress');
+  await expect(page).toHaveTitle(/Atomizer/);
+});
+
+
